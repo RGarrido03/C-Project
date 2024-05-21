6d3e99ac6973ec8758db08c10d41e82b7db0a6ac
falcaodiogo <falcao.diogo@ua.pt>
2024-05-21 13:37:04 +0100

    Add Compiler and SemanticAnalysis visitors

---
+++ src/compiler/SemanticAnalysis.java
@@ -0,0 +1,219 @@
+@SuppressWarnings("CheckReturnValue")
+public class SemanticAnalysis extends pdrawBaseVisitor<Boolean> {
+
+   @Override public Boolean visitMain(pdrawParser.MainContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitStatement(pdrawParser.StatementContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitInstructionMoveAction(pdrawParser.InstructionMoveActionContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitInstructionPenAction(pdrawParser.InstructionPenActionContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitAssignment(pdrawParser.AssignmentContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitCast(pdrawParser.CastContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitPause(pdrawParser.PauseContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitStdout(pdrawParser.StdoutContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitStderr(pdrawParser.StderrContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExecute(pdrawParser.ExecuteContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitCreateCanvas(pdrawParser.CreateCanvasContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitCreatePen(pdrawParser.CreatePenContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitClassProps(pdrawParser.ClassPropsContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitObject(pdrawParser.ObjectContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitVariable(pdrawParser.VariableContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprAddSub(pdrawParser.ExprAddSubContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprPow(pdrawParser.ExprPowContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprCast(pdrawParser.ExprCastContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprParent(pdrawParser.ExprParentContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprUnary(pdrawParser.ExprUnaryContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprFloat(pdrawParser.ExprFloatContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprStdIn(pdrawParser.ExprStdInContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprInteger(pdrawParser.ExprIntegerContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprId(pdrawParser.ExprIdContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitExprMultDivMod(pdrawParser.ExprMultDivModContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitStdin(pdrawParser.StdinContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitTuple(pdrawParser.TupleContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitDegree(pdrawParser.DegreeContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitRadian(pdrawParser.RadianContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitForward(pdrawParser.ForwardContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitBackward(pdrawParser.BackwardContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitLeft(pdrawParser.LeftContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitRight(pdrawParser.RightContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitDown(pdrawParser.DownContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitUp(pdrawParser.UpContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public Boolean visitTypeCast(pdrawParser.TypeCastContext ctx) {
+      Boolean res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+}
