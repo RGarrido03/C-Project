e6cf2335e176ad81dadf1f8c209d016ae6ea1868
falcaodiogo <falcao.diogo@ua.pt>
2024-05-22 11:38:44 +0100

    Add float type

---
+++ src/compiler/types/FloatType.java
@@ -0,0 +1,13 @@
+package types;
+
+public class FloatType extends Type{
+
+    public FloatType() {
+        super("float");
+      }
+    
+      public boolean isNumeric() {
+        return true;
+      }
+    
+}
